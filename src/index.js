18a101d263b1ee4d0e2c74c54afd6dae73731f0d
8Bts <mcmizze@yahoo.com>
2020-09-24 18:54:43 +0400

    Add bootstrap loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+/* eslint-disable linebreak-style */
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.css';
 import './styles/css-reset.css';
-import Background from './images/jason-leung.jpg';
-
-console.log(document.getElementById('content'))
+import './images/jason-leung.jpg';
---
+++ src/page-load.js
@@ -0,0 +1,5 @@
+/* eslint-disable linebreak-style */
+const load = () => {
+  const header = document.createElement('header');
+  header.innerHTML = '';
+};
